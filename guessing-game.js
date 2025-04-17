ef11839fcbc1b8daa38250873e11d4e0957c9943
sashaij <o.zagorodniuk@outlook.com>
2025-04-17 14:05:44 +0300

    Add readline interface and check guess function
    Checkguess serves for outputing the response for user input

--- guessing-game.js
+++ guessing-game.js
@@ -1,5 +1,31 @@
-let secretNumber = 1;
+let secretNumber = 12;
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 function checkGuess (number) {
 
+    if (Number(number) > secretNumber) {
+        console.log('Too high!');
+        return false;
+    } else if (Number(number) < secretNumber) {
+        console.log('Too low!');
+        return false;
+    } else if (Number(number) === secretNumber) {
+        console.log('Correct!');
+        return true;
+    }
 }
+
+function askGuess () {
+    rl.question('Enter a guess: ', answer => {
+        checkGuess(answer);
+        rl.close();
+    });
+}
+
+askGuess();
